a32bc26199d2d7e31608c7efaf9d91f3fac551d3
corze73 <137866017+corze73@users.noreply.github.com>
2025-06-29 15:47:26 +0100

    Add PWA icon files

--- vite.config.ts
+++ vite.config.ts
@@ -17,14 +17,14 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'beam-icon.svg',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/svg+xml'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'beam-icon.svg',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/svg+xml'
           }
         ]
       },
